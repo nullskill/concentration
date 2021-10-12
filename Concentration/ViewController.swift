712443eb87076cef45b59b852d6ede2b5884c026
nullskill <nullskill@github.com>
2021-10-12 22:42:50 +0300

    Lecture 2

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,7 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var game = ConcentrationGame(numberOfCardPairs: (buttonCollection.count + 1) / 2)
+    lazy var game = ConcentrationGame(numberOfCardPairs: numberOfCardPairs)
+    
+    var numberOfCardPairs: Int {
+        return (buttonCollection.count + 1) / 2
+    }
     
     var touches = 0 {
         didSet {
